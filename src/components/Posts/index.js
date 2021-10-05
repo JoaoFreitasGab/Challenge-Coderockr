a506ea348c10d4cf6fe802285188268458898835
João Gabriel Camargo de Freitas <83920066+JoaoFreitasGab@users.noreply.github.com>
2021-10-05 09:50:05 -0300

    Add files via upload

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,24 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router';
 import { useSelector } from 'react-redux'
 
-const Posts = ({ match }) => {
-  const { postId } = match.params
+const Posts = (  ) => {
+  const { postId } = useParams();
 
-  const post = useSelector(state => state.posts.find(post => post.id === postId)
-  )
+  
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
 
   return (
     <section>
       <article>
-        <h2>{post.author}</h2>
+        <h2>O Id do post é {postId}</h2>
       </article>
     </section>
   )
